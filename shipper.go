b84ee6a4623f9084c453eab3b417a339a8607880
Yusuf the Dragon <yusufardi96@gmail.com>
2021-01-24 05:00:55 +0700

    Adding Main File

---
+++ shipper.go
@@ -0,0 +1,26 @@
+package shipper
+
+// Configuration is struct for API config.
+type Configuration struct {
+	APIKey  string
+	BaseURL string
+}
+
+// Conf is instance of Configuration struct.
+var Conf = Configuration{
+	BaseURL: "https://api.sandbox.shipper.id/public/v1",
+}
+
+// SetAPIKey sets the API Key for API Call.
+func (c *Configuration) SetAPIKey(token string) *Configuration {
+	c.APIKey = token
+
+	return c
+}
+
+// SetProductionMode sets the API base URL to production.
+func (c *Configuration) SetProductionMode(productionMode bool) {
+	if productionMode {
+		c.BaseURL = "https://api.shipper.id/prod/public/v1"
+	}
+}
