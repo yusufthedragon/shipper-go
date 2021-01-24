1d07a1f8b80061d78fc5ada7e7bc97eb00f34c67
Yusuf the Dragon <yusufardi96@gmail.com>
2021-01-24 18:32:04 +0700

    Adding Unit Test for Tracking

---
+++ tracking/functions_test.go
@@ -0,0 +1,42 @@
+package tracking
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/yusufthedragon/shipper-go"
+)
+
+func init() {
+	var err = godotenv.Load("../.env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file.")
+	}
+
+	productionMode, errParse := strconv.ParseBool(os.Getenv("PRODUCTION_MODE"))
+
+	if errParse != nil {
+		productionMode = false
+	}
+
+	shipper.Conf.SetAPIKey(os.Getenv("API_KEY")).SetProductionMode(productionMode)
+}
+
+func TestGetAllStatus(t *testing.T) {
+	allStatus, err := GetAllStatus()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s, _ := json.MarshalIndent(allStatus, "", "\t")
+
+	fmt.Println("All Tracking Status:")
+	fmt.Println(string(s))
+}
